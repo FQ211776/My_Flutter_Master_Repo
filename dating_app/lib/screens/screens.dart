c25f82f48e09c8ddcccdfc5f47470a13457bf60a
Roberto Flores <fq211776@alumno.udb.edu.sv>
2022-05-21 17:12:54 -0600

    May 21, 2022 5:12 PM

--- dating_app/lib/screens/screens.dart
+++ dating_app/lib/screens/screens.dart
@@ -2,5 +2,3 @@ export 'home/home_screen.dart';
 export 'user/user_screen.dart';
 export 'matches/matches_screen.dart';
 export 'chat/chat_screen.dart';
-export 'onboarding/onboarding_screen.dart';
-export 'profile/profile_screen.dart';
