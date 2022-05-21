7387f332ce4dc02d3883e796f3cd02b4bca53f41
Roberto Flores <fq211776@alumno.udb.edu.sv>
2022-05-21 17:11:22 -0600

    May 21, 2022 5:11 PM

--- dating_app/lib/screens/home/home_screen.dart
+++ dating_app/lib/screens/home/home_screen.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/';
 
+  static Route route() {
+    return MaterialPageRoute(
+        settings: RouteSettings(name: routeName),
+        builder: (context) {
+          print("Hola");
+          return Scaffold(
+	  body: Center(
+		child: Text('Hola Mundo'),
+	 ),
+   );
+
+        });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
-    );
+	return Scaffold(
+	  body: Center(
+		child: Text('Hola Mundo'),
+	 ),
+   );
   }
 }
